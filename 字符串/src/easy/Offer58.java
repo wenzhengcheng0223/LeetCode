ed60ecc02f146e73f50db806c720f9d700acf2e1
wenzhengcheng0223 <wenzhengcheng0223@163.com>
2021-08-22 14:36:34 +0800

    新增 剑指 Offer 58 - II. 左旋转字符串

---
+++ 字符串/src/easy/Offer58.java
@@ -0,0 +1,35 @@
+package easy;
+
+/**
+ * @Description :
+ * @Author : WenZhengcheng
+ * @Date : create in 2021-08-22 下午 02:22
+ * @Email : wenzhengcheng0223@163.com
+ * @Since : JDK 1.8
+ * @PackageName : easy
+ * @ProjectName : LeetCode
+ * @Version : 1.0.0
+ */
+public class Offer58 {
+    class Solution {
+        /**
+         * 列表遍历拼接
+         * @param s
+         * @param n
+         * @return
+         */
+        public String reverseLeftWords(String s, int n) {
+            if(n == 0){
+                return s;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = n; i < s.length(); i++) {
+                stringBuilder.append(s.charAt(i));
+            }
+            for (int i = 0;i <n;i++){
+                stringBuilder.append(s.charAt(i));
+            }
+            return stringBuilder.toString();
+        }
+    }
+}
